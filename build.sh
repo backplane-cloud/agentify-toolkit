13cc2c82b2a0f164d580f89e91045d94a97de7a7
Lewis Sheridan <lewis@backplane.cloud>
2026-01-10 03:05:06 +0100

    fixing path on src/*.egg-info in build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ command -v python3 >/dev/null 2>&1 || { echo >&2 "python3 not found."; exit 1; }
 command -v twine >/dev/null 2>&1 || { echo >&2 "twine not found."; exit 1; }
 
 echo "Cleaning build artifacts..."
-rm -rf dist/ build/ *.egg-info
+rm -rf dist/ build/ src/*.egg-info
 
 echo "Building wheel + sdist..."
 python3 -m build
