b33c89bd98d5fd3534111264de3f6c3c04161b59
Lewis Sheridan <lewis@backplane.cloud>
2026-02-02 01:18:44 +0100

    tidying up examples

---
+++ examples/demo_internal_cli_sdk/tools/multiply_numbers.py
@@ -0,0 +1,12 @@
+"""
+Internal tool: multiply two numbers
+"""
+
+def multiply_numbers(a: float, b: float) -> float:
+    """
+    Multiply two numbers and returns the result.
+
+    Example:
+        multiply_numbers(a=2, b=3) -> 15
+    """
+    return a * b
