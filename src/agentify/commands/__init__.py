09e6f3004f35443b3b3761c615ddc56c19447233
Lewis Sheridan <lewis@backplane.cloud>
2026-02-28 22:32:45 +0100

    removing original mcp rest api implementation

--- src/agentify/commands/__init__.py
+++ src/agentify/commands/__init__.py
@@ -6,7 +6,6 @@ from .agent import agent_group
 from .provider import provider_group
 from .deploy import deploy_command
 from .gateway import gateway_command
-from ._mcp import xmcp_group
 from .mcp import mcp_group
 
 __all__ = [
@@ -18,6 +17,5 @@ __all__ = [
     "agent_group",
     "provider_group",
     "gateway_command",
-    "mcp_group",
-    "xmcp_group"
+    "mcp_group"
 ]
